88d4af1892edba1123b4b30eae17c8f9b7ab9fdd
osborn roxas <osbornroxas02@gmail.com>
2018-04-26 18:11:33 -0700

    js scroll elements

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	
 	/* For the sticky navigation */
+	
 	$('.js--section-features').waypoint(function(direction) {
 		if (direction == "down") {
 			$('nav').addClass('sticky');
@@ -21,6 +22,25 @@ $(document).ready(function() {
 		$('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
 	});
 	
+	
+	
+	/* Navigation Scroll */
+	
+	$(function() {
+		$('a[href*=#]:not([href=#])').click(function() {
+			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+				if (target.length) {
+					$('html,body').animate({
+						scrollTop: target.offset().top
+					}, 1000);
+					return false;
+				}
+			}
+		});
+	});
+
 	
 });
 				  
