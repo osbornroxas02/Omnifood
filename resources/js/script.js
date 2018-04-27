b4f17442887c7b4f4dc789d67f542a48c9e2d9cc
osborn roxas <osbornroxas02@gmail.com>
2018-04-27 08:53:56 -0700

    js responive nav

--- resources/js/script.js
+++ resources/js/script.js
@@ -42,6 +42,45 @@ $(document).ready(function() {
 	});
 
 	
+	/* Animation on scroll */
+	
+	$('.js--wp-1').waypoint(function(direction) {
+		$('.js--wp-1').addClass('animated fadeIn');
+	}, {
+		offset: '50%'
+	});
+	
+	$('.js--wp-2').waypoint(function(direction) {
+		$('.js--wp-2').addClass('animated fadeInUp');
+	}, {
+		offset: '50%'
+	});
+	
+	$('.js--wp-3').waypoint(function(direction) {
+		$('.js--wp-3').addClass('animated fadeIn');
+	}, {
+		offset: '50%'
+	});
+	
+	$('.js--wp-4').waypoint(function(direction) {
+		$('.js--wp-4').addClass('animated pulse');
+	}, {
+		offset: '50%'
+	});
+	
+	
+	
+	/* Mobile navigation */
+	
+	$('.js--nav-icon').click(function() {
+		var nav = $('.js--main-nav'); /*create a var nav so you do have to repeat*/
+		
+		nav.slideToggle(200)/*create the open close effect*/
+	}); 
+	
+	
+	
+	
 });
 				  
 				  
